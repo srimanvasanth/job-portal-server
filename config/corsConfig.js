b2c2f5858e59fd68a17f54f7f7314187f68b7e11
Sriman Vasanth <147894546+srimanvasanth@users.noreply.github.com>
2026-04-30 16:28:26 +0530

    Update corsConfig.js

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,5 +1,5 @@
 const corsConfig = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "https://job-portal-client-1ppa.onrender.com");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Origin, Accept, Authorization, Content-Type");
