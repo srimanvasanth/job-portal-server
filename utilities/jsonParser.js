86143f28eacdd2c4886ac095613fef58cbb46c99
Sriman Vasanth <147894546+srimanvasanth@users.noreply.github.com>
2026-04-30 16:07:35 +0530

    Update jsonParser.js

--- utilities/jsonParser.js
+++ utilities/jsonParser.js
@@ -2,11 +2,15 @@ const fs = require("fs").promises;
 const connectDB = require("../config/dbConfig");
 const codingQuestions = require("../modal/codingQuestions");
 const jobDetails = require("../modal/jobDetails");
+const path = require("path");
+
+const questionsFilePath = path.join(__dirname, "utilities", "coding-questions.txt");
+const jobDetailsFilePath = path.join(__dirname, "utilities", "job-details.txt");
 
 const readQuestions = async () => {
     try {
         // path for debug "./server/utilities/coding-questions.txt"
-        const data = await fs.readFile('./utilities/coding-questions.txt', 'utf-8');
+        const data = await fs.readFile(questionsFilePath, 'utf-8');
         const parsedData = JSON.parse(data);
         return parsedData;
     } catch (error) {
@@ -18,7 +22,7 @@ const readQuestions = async () => {
 const readJobData = async () => {
     try {
         // path for debug "./server/utilities/coding-questions.txt"
-        const data = await fs.readFile('./utilities/job-details.txt', 'utf-8');
+        const data = await fs.readFile(jobDetailsFilePath, 'utf-8');
         const parsedData = JSON.parse(data);
         return parsedData;
     } catch (error) {
@@ -32,6 +36,7 @@ const insertQuestionsToDB = async () => {
         await connectDB();
         const questionData = await readQuestions();
         const jobData = await readJobData();
+        console.log(jobData)
         const newData = questionData.questions.map(q => ({...q,  options: JSON.parse(q.options.replace(/'/g, '"'))}));
         if(questionData?.insert){
             await codingQuestions.deleteMany({});
